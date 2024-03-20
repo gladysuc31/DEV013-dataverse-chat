4ea80764e791ffd2063347b1a8c179b14a63d4bc
uzy3090 <lucia1688@hotmail.com>
2024-03-19 20:54:43 -0600

    vistas

--- src/views/Chat.js
+++ src/views/Chat.js
@@ -1,7 +1,28 @@
 // import { navigateTo } from "../router.js";
 
-export const Chat = () => {
-  const viewEl = document.createElement("div");
-  viewEl.innerHTML = "I'm the Chat Page";
-  return viewEl;
-};
+export const Chat = () => { 
+
+  const chatDiv = document.createElement("div"); 
+
+  chatDiv.classList.add("chatDiv"); 
+
+chatDiv.innerHTML =  
+
+`<section class="chat"> 
+
+<div class="container"> 
+
+        <textarea placeholder="Escribe aquí..."></textarea> 
+
+        <p><button type="button">Enviar</button></p> 
+
+        <button>Regresar</button> 
+
+</section>`; 
+return chatDiv; 
+
+} 
+
+ 
+
+ 
