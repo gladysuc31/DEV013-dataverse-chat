9e76f96bab20a4cc8edeabcf663ce2f4505b39cb
uzy3090 <lucia1688@hotmail.com>
2024-03-28 09:06:21 -0600

    openAIA

--- src/lib/openAIApi.js
+++ src/lib/openAIApi.js
@@ -1,7 +1,34 @@
 // Importa la función para obtener la API KEY desde apiKey.js
-//import { getApiKey } from './apiKey.js';
-
+import { getApiKey } from './apiKey.js';
 
 /*export const communicateWithOpenAI = (messages) => {
-   //Aquí es donde debes implementar la petición con fetch o axios
+/Aquí es donde debes implementar la petición con fetch o axios
 }; */
+
+export const communicateWithOpenAI= async (pelicula, input) =>{
+   const apiKey = getApiKey ();
+   const apiUrl = "https://api.openai.com/v1/chat/completions";
+ 
+   const responseIA = fetch(apiUrl, {
+      method: "POST",
+      Headers: {
+         "Content-Type": "application/json",
+         "Authorization": `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+         model: "gpt-3.5-turbo",
+         messages: [
+            {
+               role:"system",
+               content: `Tu eres una película de Pixar ${pelicula} y debes comportarte amigable, divertido, 
+               educativo, con valores, puedes compartir tus facts mas destacados y debes responder en primera persona.`
+             },
+             {
+               role: "user",
+               content: input,
+             },
+         ],
+      }),
+   });
+   return responseIA; 
+}; 
